acf00543b4a7f8a6d93dc865ae24324c2b1f9a41
mukhlis <mukhlisk93@gmail.com>
2017-01-10 16:35:14 +0700

    Add form in Setting.js

--- src/admin/pages/Settings.js
+++ src/admin/pages/Settings.js
@@ -16,7 +16,74 @@ var Settings = React.createClass({
 			    </section>
 
 			    <section className="content">
+			    	<div className="row">
+					  	<div className="col-md-6">
+					  	<section className="content">
+			    			<form className="form-horizontal">
+			    			
+					  			<div className="form-group">
+								  	<label for="name" className="col-md-3">Name</label>
+								  	<div className="col-md-9">
+										<input type="text" name="name" placeholder="Name" className="form-control" />
+										<p><i>Fill in your Name</i></p>
+									</div>
+								</div>
+
+					  			<div className="form-group">
+								  	<label for="tagline" className="col-md-3">Tagline</label>
+								  	<div className="col-md-9">
+										<input type="text" name="tagline" placeholder="Tagline" className="form-control" />
+										<p><i>Fill in your Tag Line</i></p>
+									</div>
+								</div>
+
+					  			<div className="form-group">
+								  	<label for="keywoards" className="col-md-3">Keywoards</label>
+								  	<div className="col-md-9">
+										<input type="text" name="keywoards" placeholder="Keywoards" className="form-control" />
+										<p><i>Fill in your Keywoard</i></p>
+									</div>
+								</div>
+
+					  			<div className="form-group">
+								  	<label for="homeUrl" className="col-md-3">Home URL</label>
+								  	<div className="col-md-9">
+										<input type="text" name="homeUrl" placeholder="Home URL" className="form-control" />
+										<p><i>Fill in your Home URL</i></p>
+									</div>
+								</div>
+
+					  			<div className="form-group">
+								  	<label for="adminEmail" className="col-md-3">Admin Email</label>
+								  	<div className="col-md-9">
+										<input type="text" name="adminEmail" placeholder="Admin Email" className="form-control" />
+										<p><i>Fill in your Email</i></p>
+									</div>
+								</div>
+
+					  			<div className="form-group">
+								  	<label for="timeZone" className="col-md-3">Time Zone</label>
+								  	<div className="col-md-9">
+										<input type="text" name="timeZone" placeholder="Time Zone" className="form-control" />
+										<p><i>Fill in your Time Zone</i></p>
+									</div>
+								</div>
+								
+								<div className="form-group">
+								<div className="col-md-3">
+									
+										<input type="submit" value="Save" className="btn btn-success btn-block btn-h1-spacing" />
+								</div>
+								</div>
+								
+							</form>
+						</section>
+					  	</div>
+					</div>
+					
 			    </section>
+
+			    
 		    </div>
 		)
 	}
