6e56e3f835e374883f91122ee18995fca570b618
Yulian Purnama <ian.doang@gmail.com>
2017-01-11 14:22:42 +0700

    convert menus from hardcoded JSX to hardcoded JSON, so we can easily store it to database later

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import client from './apollo';
 import Theme from './theme';
 import Admin from './admin';
 
-render((
-	<ApolloProvider client={client}>
-		<BrowserRouter>
-			<div id="router" style={{height: "100%"}}>
-				<Match pattern="/admin/:page?/:b?/:c?/:d?/:e?/:f?/:g?/:h?/:i?/:j?" component={Admin}/>
-				<Miss component={Theme}/>
-			</div>
-		</BrowserRouter>
-	</ApolloProvider>
-), document.getElementById('root'))
+const Main = React.createClass({
+	render: function(){
+		return (
+			<ApolloProvider client={client}>
+				<BrowserRouter>
+					<div id="router" style={{height: "100%"}}>
+						<Match pattern="/admin/:page?/:action?/:param1?/:param2?/:param3?/:param4?/:param5?" component={Admin}/>
+						<Miss component={Theme}/>
+					</div>
+				</BrowserRouter>
+			</ApolloProvider>
+		)
+	}
+});
+
+render(<Main/>, document.getElementById('root'));
