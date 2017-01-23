99d32255a0bbab8161d98b4c98457e6c60ec23aa
Yulian Purnama <ian.doang@gmail.com>
2017-01-23 19:28:56 +0700

    show error page when theme is invalid

--- src/admin/theme.js
+++ src/admin/theme.js
@@ -1,27 +1,88 @@
 import $ from 'jquery';
 window.jQuery = $;
+
 import React from 'react';
 import Config from '../config';
 window.config = Config;
 
+import 'jquery-ui/ui/core';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const InvalidTheme = React.createClass({
+	componentDidMount: function(){
+		require ('../../public/css/AdminLTE.css');
+		require ('../../public/css/skins/_all-skins.css');	
+	},
+	render: function() {
+		return (
+			<div className="content-wrapper" style={{minHeight: 1126}}>
+			    <section className="content-header">
+			      <h1>
+			        Error Page
+			      </h1>
+			    </section>
+
+			    <section className="content">
+
+			      <div className="error-page">
+
+			        <div className="error-content">
+			          <h3><i className="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
+
+			          <p>
+			            We will work on fixing that right away.
+			          </p>
+
+			        </div>
+			      </div>
+
+			    </section>
+			  </div>
+		)
+	}
+});
+
+function getTemplateComponent(type){
+	var c = window.config.theme;
+	
+	if (c.name==null || c.path==null) {
+		return InvalidTheme;
+	}
+
+	try {
+		let Component = require('../theme/'+c.path+'/index.js').default;
+		if (type==="home") {
+			// pass
+		} else if (type==="post") {
+			Component = require('../theme/'+c.path+'/layouts/Post.js').default;			
+		} else if (type==="page") {
+			Component = require('../theme/'+c.path+'/layouts/Page.js').default;			
+		}
+
+		return Component;
+	} catch(e) {
+		return InvalidTheme;
+	}
+}
+
 const ThemeHome = React.createClass({
 	render: function() {
-		let Theme = require('../theme/'+window.config.theme.path+'/index.js').default;
-		//return <div>OK</div>;
-		return <Theme />
+		let Home = getTemplateComponent('home');
+		return <Home />
 	}
 });
 
 const ThemeSingle = React.createClass({
 	render: function() {
-		let Post = require('../theme/'+window.config.theme.path+'/layouts/Post.js').default;
+		let Post = getTemplateComponent('post');
 		return <Post/>;
 	}
 });
 
 const ThemePage = React.createClass({
 	render: function() {
-		let Page = require('../theme/'+window.config.theme.path+'/layouts/Page.js').default;
+		let Page = getTemplateComponent('page');
 		return <Page/>;
 	}
 });
