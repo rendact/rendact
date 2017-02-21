596f0ad62baa6bfac220f7d7d39fc1852f67ea9a
Yulian Purnama <ian.doang@gmail.com>
2017-02-21 11:19:31 +0700

    enhance "not found" page

--- src/admin/NotFound.js
+++ src/admin/NotFound.js
@@ -1,19 +1,49 @@
 import React from 'react';
 
 var NotFound = React.createClass({
+	componentDidMount: function(){
+		require ('../../public/css/AdminLTE.css');
+		require ('../../public/css/skins/_all-skins.css');
+	},
 	render: function(){
 		return (
-			<div className="content-wrapper" style={{height: '100%'}}>
-				<section className="content-header">
-			      <h1>
-			        Content not found
-			      </h1>
-			    </section>
+			<div className="content-wrapper-full" style={{minHeight: 1126}}>
+		    <section className="content-header">
+		      <h1>
+		        404 Error Page
+		      </h1>
+		      <ol className="breadcrumb">
+		        <li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
+		        <li className="active">404 error</li>
+		      </ol>
+		    </section>
 
-			    <section className="content">
-			    	<p>Content your are trying to access is not found</p>
-			    </section>
-		    </div>
+		    <section className="content">
+		      <div className="error-page">
+		        <h2 className="headline text-yellow"> 404</h2>
+
+		        <div className="error-content">
+		          <h3><i className="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+
+		          <p>
+		            We could not find the page you were looking for.
+		            Meanwhile, you may <a href="../../index.html">return to dashboard</a> or try using the search form.
+		          </p>
+
+		          <form className="search-form">
+		            <div className="input-group">
+		              <input type="text" name="search" className="form-control" placeholder="Search"/>
+
+		              <div className="input-group-btn">
+		                <button type="submit" name="submit" className="btn btn-warning btn-flat"><i className="fa fa-search"></i>
+		                </button>
+		              </div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+		    </section>
+		  </div>
 		)
 	}
 });
--- src/admin/theme.js
+++ src/admin/theme.js
@@ -8,35 +8,43 @@ import Query from './query';
 import 'jquery-ui/ui/core';
 import 'bootstrap/dist/css/bootstrap.css';
 
+const LoadingPage = React.createClass({
+	componentDidMount: function(){
+		require ('../../public/css/AdminLTE.css');
+		require ('../../public/css/skins/_all-skins.css');	
+		require ('font-awesome/css/font-awesome.css');
+		require ('../../public/css/ionicons.min.css');
+	},
+	render: function() {
+		return (
+			<div>
+			<h2 style={{marginTop:100,width:"100%",textAlign:"center"}}><i className="fa fa-spin fa-refresh"></i> Loading page...</h2>
+			</div>
+		)
+	}
+});
 
 const InvalidTheme = React.createClass({
 	componentDidMount: function(){
 		require ('../../public/css/AdminLTE.css');
 		require ('../../public/css/skins/_all-skins.css');	
+		require ('font-awesome/css/font-awesome.css');
+		require ('../../public/css/ionicons.min.css');
 	},
 	render: function() {
 		return (
 			<div className="content-wrapper" style={{minHeight: 1126}}>
 			    <section className="content-header">
-			      <h1>
-			        Error Page
-			      </h1>
+			      <h1>Error Page</h1>
 			    </section>
 
 			    <section className="content">
-
 			      <div className="error-page">
-
 			        <div className="error-content">
 			          <h3><i className="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
-
-			          <p>
-			            We will work on fixing that right away.
-			          </p>
-
+			          <p>We will work on fixing that right away.</p>
 			        </div>
 			      </div>
-
 			    </section>
 			  </div>
 		)
@@ -69,22 +77,22 @@ function getTemplateComponent(type){
 const ThemeHome = React.createClass({
 	getInitialState: function(){
 		return {
+			loadDone: false,
 			isSlugExist: false,
-			slug: null
+			slug: this.props.location.pathname.replace("/","")
 		}
 	},
 	componentWillMount: function(){
 		var me = this;
-		var slug = this.props.location.pathname.replace("/","");
+
 		request({
       url: Config.scapholdUrl,
       method: "POST",
       json: true,
       headers: {
-        "content-type": "application/json",
-        "Authorization": "Bearer " + localStorage.token
+        "content-type": "application/json"
       },
-      body: Query.checkSlugQry(slug)
+      body: Query.checkSlugQry(this.state.slug)
     }, (error, response, body) => {
       if (!error && !body.errors && response.statusCode === 200) {
         var slugCount = body.data.viewer.allPosts.edges.length;
@@ -94,7 +102,7 @@ const ThemeHome = React.createClass({
       } else {
         me.setState({errorMsg: "error when checking slug"});
       }
-      me.setState({slug: slug});
+      me.setState({loadDone: true});
     });
 	},
 	componentDidMount: function(){
@@ -104,15 +112,19 @@ const ThemeHome = React.createClass({
 		require('../theme/'+c.path+'/functions.js');
 	},
 	render: function() {
-		if (this.state.slug){
-			let Single = getTemplateComponent('single');
-			if (this.state.isSlugExist)
-				return <Single slug={this.state.slug} />
-			else 
-				return <NotFound/>
+		if (!this.state.loadDone && this.state.slug) {
+			return <LoadingPage/>
 		} else {
-			let Home = getTemplateComponent('home');
-			return <Home />
+			if (this.state.slug){
+				let Single = getTemplateComponent('single');
+				if (this.state.isSlugExist)
+					return <Single slug={this.state.slug} />
+				else 
+					return <NotFound/>
+			} else {
+				let Home = getTemplateComponent('home');
+				return <Home />
+			}
 		}
 	}
 });
