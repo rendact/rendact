b4b71047ffcacc1a7c410211fd571f66638866db
Yulian Purnama <ian.doang@gmail.com>
2017-04-11 00:52:28 +0700

    NO ACCESS — Better styling and centered vertically also. There is a scroll and bottom style problem see: http://i.imgur.com/AuJcvTv.png

--- src/admin/NotPermissible.js
+++ src/admin/NotPermissible.js
@@ -2,32 +2,21 @@ import React from 'react';
 
 var NotPermissible = React.createClass({
 	componentDidMount: function(){
-		require ('../../public/css/AdminLTE.css');
-		require ('../../public/css/skins/_all-skins.css');
+		
 	},
 	render: function(){
 		return (
-			<div className="content-wrapper-full" style={{minHeight: 1126}}>
-		    <section className="content-header">
-		      <h1>
-		        No Priveledges
-		      </h1>
-		      <ol className="breadcrumb">
-		        <li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
-		        <li className="active">No priveledges</li>
-		      </ol>
-		    </section>
-
-		    <section className="content">
-		      <div className="error-page">
-		        <div className="error-content">
-		          <p>
-		            You don't have priveledges to access this page
-		          </p>
-		        </div>
-		      </div>
-		    </section>
-		  </div>
+			<div className="content-wrapper">
+				<div className="content-fluid">
+					<section className="content">
+						<div className="row">
+						  <h3 className="text-center">
+						    You don't have permission to access this page
+						  </h3>
+						</div>
+					</section>
+				</div>
+			</div>
 		)
 	}
 });
