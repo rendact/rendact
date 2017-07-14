3b97cca786c93e290a7b8ed730840177a7c1a223
Yulian Purnama <ian.doang@gmail.com>
2017-07-14 17:05:32 +0700

    Successfully implementing redux-form in Settings.js

--- src/admin/pages/Settings.js
+++ src/admin/pages/Settings.js
@@ -5,143 +5,36 @@ import Halogen from 'halogen'
 import Query from '../query'
 import {riques, errorCallback, getFormData, disableForm, defaultHalogenStyle} from '../../utils'
 import {connect} from 'react-redux'
-import {maskArea} from '../../actions'
-import { reduxForm } from 'redux-form'
-
-let SettingsForm = (props) => {
-  const { handleSubmitBtn, style } = props
-  return (
-  	<form onSubmit={handleSubmitBtn} className="form-horizontal" style={style} >
-			    			
-  		<div className="form-group">
-			  	<label htmlFor="name" className="col-md-3">Website name</label>
-			  	<div className="col-md-9">
-					<input type="text" name="name" className="form-control rdt-input-form" />
-					<p className="help-block">Website name, will shows up in <code>title</code> tag</p>
-				</div>
-			</div>
-
-  			<div className="form-group">
-			  	<label htmlFor="tagline" className="col-md-3">Tagline</label>
-			  	<div className="col-md-9">
-					<input type="text" name="tagline" className="form-control rdt-input-form" />
-					<p className="help-block">Few words that describes your web, example: a bunch of words of mine</p>
-				</div>
-			</div>
-
-  			<div className="form-group">
-			  	<label htmlFor="keywoards" className="col-md-3">Keywords</label>
-			  	<div className="col-md-9">
-					<input type="text" name="keywords" className="form-control rdt-input-form" />
-					<p className="help-block">Some words represents your web</p>
-				</div>
-			</div>
-
-  		<div className="form-group">
-			 	<label htmlFor="rootUrl" className="col-md-3">Home URL</label>
-		  	<div className="col-md-9">
-					<input type="text" name="rootUrl" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-  		<div className="form-group">
-			  	<label htmlFor="adminEmail" className="col-md-3">Admin Email</label>
-			  	<div className="col-md-9">
-					<input type="text" name="adminEmail" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-  		<div className="form-group">
-			  	<label htmlFor="timeZone" className="col-md-3">Time Zone</label>
-			  	<div className="col-md-9">
-					<input type="text" name="timeZone" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="dbApiUrl" className="col-md-3">Database API URL</label>
-		  	<div className="col-md-9">
-					<input type="text" name="dbApiUrl" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="auth0ClientId" className="col-md-3">Auth0 Client ID</label>
-		  	<div className="col-md-9">
-					<input type="text" name="auth0ClientId" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="auth0Domain" className="col-md-3">Auth0 Domain</label>
-		  	<div className="col-md-9">
-					<input type="text" name="auth0Domain" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="mailApiUrl" className="col-md-3">Email API URL</label>
-		  	<div className="col-md-9">
-					<input type="text" name="mailApiUrl" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="mailApiKey" className="col-md-3">Email API Key</label>
-		  	<div className="col-md-9">
-					<input type="text" name="mailApiKey" className="form-control rdt-input-form" />
-				</div>
-			</div>
-
-			<div className="form-group">
-			 	<label htmlFor="mailDefaultSender" className="col-md-3">Email Default Sender</label>
-		  	<div className="col-md-9">
-					<input type="text" name="mailDefaultSender" className="form-control rdt-input-form" />
-				</div>
-			</div>
-			
-			<div className="form-group">
-					<div className="col-md-9">
-						<div className="btn-group">
-							<input type="submit" value="Update Settings" className="btn btn-primary btn-sm" />
-						</div>
-					</div>
-				</div>
-		</form>
-  )
-}
-
-SettingsForm = reduxForm({
-  form: 'settingsForm'
-})(SettingsForm)
+import {maskArea, loadFormData} from '../../actions'
+import {reduxForm, Field} from 'redux-form'
 
 var Settings = React.createClass({
 	propTypes: {
 		isProcessing: React.PropTypes.bool.isRequired,
 		opacity: React.PropTypes.number.isRequired,
-		doMaskArea: React.PropTypes.func.isRequired
+		data: React.PropTypes.object
 	},
 	getDefaultProps: function() {
   	return {
 			isProcessing: false,
-      opacity: 1
+      opacity: 1,
+      data: {}
 		}
  	},
 	loadData: function(){
 		var qry = Query.loadSettingsQry;
 		var me = this;
-		this.props.doMaskArea(true)
+		this.props.dispatch(maskArea(false));
 		riques(qry, 
 			function(error, response, body){
 				if(!error && !body.errors) {
+					var _data = {}
 					_.forEach(body.data.viewer.allOptions.edges, function(item){
 						var _el = document.getElementsByName(item.node.item);
-						if (_el.length>0){
-							_el[0].value = item.node.value;
-							_el[0].id = item.node.id;
-						}
+						_data[item.node.item] = item.node.value;
 					});
-					me.props.doMaskArea(false)
+					me.props.dispatch(loadFormData(_data));
+					me.props.dispatch(maskArea(false));
 				} else {
 					errorCallback(error, body.errors?body.errors[0].message:null);
 				}
@@ -150,7 +43,7 @@ var Settings = React.createClass({
 	},
 	disableForm: function(state){
     disableForm(state, this.notification);
-    this.props.doMaskArea(state)
+    this.props.dispatch(maskArea(false));
   },
 	handleSubmitBtn: function(event){
 		event.preventDefault();
@@ -196,7 +89,103 @@ var Settings = React.createClass({
 			    	<div className="row">
 					  	<div className="col-md-8">
 					  	<section className="content">
-			    			<SettingsForm onSubmit={this.handleSubmitBtn} style={{opacity: this.props.opacity}}/>
+			    			<form onSubmit={this.handleSubmitBtn} className="form-horizontal" style={{opacity: this.props.opacity}} >
+			    			
+						  		<div className="form-group">
+									  	<label htmlFor="name" className="col-md-3">Website name</label>
+									  	<div className="col-md-9">
+									  	<Field name="name" component="input" type="text" className="form-control"/>
+											<p className="help-block">Website name, will shows up in <code>title</code> tag</p>
+										</div>
+									</div>
+
+						  			<div className="form-group">
+									  	<label htmlFor="tagline" className="col-md-3">Tagline</label>
+									  	<div className="col-md-9">
+									  	<Field name="tagline" component="input" type="text" className="form-control"/>
+											<p className="help-block">Few words that describes your web, example: a bunch of words of mine</p>
+										</div>
+									</div>
+
+						  			<div className="form-group">
+									  	<label htmlFor="keywoards" className="col-md-3">Keywords</label>
+									  	<div className="col-md-9">
+									  	<Field name="keywords" component="input" type="text" className="form-control"/>
+											<p className="help-block">Some words represents your web</p>
+										</div>
+									</div>
+
+						  		<div className="form-group">
+									 	<label htmlFor="rootUrl" className="col-md-3">Home URL</label>
+								  	<div className="col-md-9">
+								  		<Field name="rootUrl" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+						  		<div className="form-group">
+								  	<label htmlFor="adminEmail" className="col-md-3">Admin Email</label>
+								  	<div className="col-md-9">
+								  		<Field name="adminEmail" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+						  		<div className="form-group">
+								  	<label htmlFor="timeZone" className="col-md-3">Time Zone</label>
+								  	<div className="col-md-9">
+								  		<Field name="timeZone" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="dbApiUrl" className="col-md-3">Database API URL</label>
+								  	<div className="col-md-9">
+								  		<Field name="dbApiUrl" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="auth0ClientId" className="col-md-3">Auth0 Client ID</label>
+								  	<div className="col-md-9">
+								  		<Field name="auth0ClientId" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="auth0Domain" className="col-md-3">Auth0 Domain</label>
+								  	<div className="col-md-9">
+								  		<Field name="auth0Domain" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="mailApiUrl" className="col-md-3">Email API URL</label>
+								  	<div className="col-md-9">
+								  		<Field name="mailApiUrl" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="mailApiKey" className="col-md-3">Email API Key</label>
+								  	<div className="col-md-9">
+								  		<Field name="mailApiKey" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+
+									<div className="form-group">
+									 	<label htmlFor="mailDefaultSender" className="col-md-3">Email Default Sender</label>
+								  	<div className="col-md-9">
+								  		<Field name="mailDefaultSender" component="input" type="text" className="form-control"/>
+										</div>
+									</div>
+									
+									<div className="form-group">
+											<div className="col-md-9">
+												<div className="btn-group">
+													<input type="submit" value="Update Settings" className="btn btn-primary btn-sm" />
+												</div>
+											</div>
+										</div>
+								</form>
 							</section>
 							</div>
 						</div>
@@ -209,16 +198,14 @@ var Settings = React.createClass({
 
 const mapStateToProps = function(state){
 	if (!_.isEmpty(state.settings)) {
-		return _.head(state.settings)
+		var out = _.head(state.settings);
+		out["initialValues"] = out.data;
+		return out;
 	} else return {}
 }
 
-const mapDispatchToProps = {
-  doMaskArea: maskArea
-}
-
-Settings = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Settings)
+Settings = reduxForm({
+  form: 'settingsForm'
+})(Settings)
+Settings = connect(mapStateToProps)(Settings)
 export default Settings;
