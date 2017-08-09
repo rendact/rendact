461d084de551ad7c62dc0439131b5cedcbeeee3f
ihfazhillah <mihfazhillah@gmail.com>
2017-08-09 13:12:03 +0700

    separate WidgetAreaContainer into file

--- src/admin/pages/Widgets/index.js
+++ src/admin/pages/Widgets/index.js
@@ -5,6 +5,7 @@ import uuid from 'uuid';
 import {riques, swalert, errorCallback} from '../../../utils';
 import Query from '../../query';
 import BoxItemAvailable from './BoxItemAvailable';
+import WidgetAreaContainer from './WidgetAreaContainer';
 
 class BoxItemSidebar extends React.Component {
 
@@ -50,53 +51,6 @@ class BoxItemSidebar extends React.Component {
 )
    }
 }
-
-
-
-class WidgetAreaContainer extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.handleClearAll = this.handleClearAll.bind(this);
-    }
-
-    handleClearAll(e){
-        e.preventDefault();
-        this.props.clearAllWidget(this.props.id);
-    }
-  render(){
-
-    return <div id={this.props.id} className="col-md-6">
-                <div className="box box-default collapsed-box">
-                    <div className="box-header with-border">
-                        <h3 className="box-title">{this.props.title}</h3>
-                        <div className="box-tools pull-right">
-                            <button className="btn btn-box-tool btn-primary" data-widget="collapse">
-                                <i className="fa fa-plus"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="box-body">
-                        <ul id="dragablePanelList" className="widgets list-unstyled">
-                        {this.props.widgetAreas && 
-                            _.map(this.props.widgets, (widget, index) => (
-                                                                <li key={index}>
-                                                                        {widget}
-                                                                </li>
-                                                                ))
-                        }
-                    </ul>
-                    </div>
-                    <div className="box-footer">
-                        <button onClick={this.handleClearAll} className="btn btn-danger">Clear All</button>
-                        <button className="btn btn-success pull-right">Save</button>
-                    </div>
-                </div>
-            </div>
-    }
-}
-
-
 
 class Widgets extends React.Component {
     constructor(props){
