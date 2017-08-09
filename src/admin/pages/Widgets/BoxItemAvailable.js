dbcd3bd7066a9c0f3e198c55fe8efe431b624c0a
ihfazhillah <mihfazhillah@gmail.com>
2017-08-09 13:07:51 +0700

    widgetItemsAvailable now can be dragged

--- src/admin/pages/Widgets/BoxItemAvailable.js
+++ src/admin/pages/Widgets/BoxItemAvailable.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import {DragSource} from 'react-dnd';
+
+
+const dragSource = {
+  beginDrag(props, monitor, component){
+    return {widget: props.widgets}
+  },
+}
+
+
+const dragCollect = (connect, monitor) => ({
+  connectDragToDom: connect.dragSource(),
+  isDragging: monitor.isDragging()
+});
 
 class BoxItemAvailable extends React.Component {
 
@@ -31,8 +45,12 @@ class BoxItemAvailable extends React.Component {
     var widget = this.props.widget;
     var widgetValue = JSON.parse(widget.value);
     var widgetAreas = this.props.widgetAreas;
+    var {connectDragToDom, isDragging} = this.props;
 
-    return <div className="box box-info box-solid" id={widget.item}>
+    let opacity = isDragging? 0.5 : 1;
+
+    return connectDragToDom(
+      <div className="box box-info box-solid" style={{opacity}} id={widget.item}>
     <div className="box-header with-border">
         <h3 className="box-title">{widgetValue.title}</h3>
     </div>
@@ -56,9 +74,12 @@ class BoxItemAvailable extends React.Component {
         </div>
     </div>
   </div>
+    )
   }
 
 }
+
+BoxItemAvailable = DragSource('available', dragSource, dragCollect)(BoxItemAvailable);
 
 
 export default BoxItemAvailable;
