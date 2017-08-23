80005eb3746defe213cacea4de266410059d10c7
ihfazhillah <mihfazhillah@gmail.com>
2017-08-23 13:27:03 +0700

    on search submit, change the url and render Dummy Search page

--- src/includes/DefaultWidgets/search.js
+++ src/includes/DefaultWidgets/search.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import _ from 'lodash'
 import {reduxForm, Field} from 'redux-form'
+import {connect} from 'react-redux'
 
 
 class Widget extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.onSearch = this.onSearch.bind(this)
+  }
+
+  onSearch(value){
+    this._reactInternalInstance._context.history.push("/search/"+value.search)
+  }
+
   render(){
-    return <form onSubmit={this.props.handleSubmit(() => {})}>
+    return <form onSubmit={this.props.handleSubmit(this.onSearch)}>
         <p>
         <Field className="form-control input" component="input" type="text" name="search" placeholder="type key to search"/>
         </p>
@@ -13,7 +24,9 @@ class Widget extends React.Component {
   }
 }
 
-Widget = reduxForm({form: 'searchForm'})(Widget)
+Widget = _.flow([
+  reduxForm({form: 'searchForm'}),
+])(Widget)
 
 const WidgetFn = (widgetData) => {
 	return (
