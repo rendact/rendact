e586daf3c619d0b565381b8bbd1972e42f7e5e07
mukhlis <mukhlisk93@gmail.com>
2017-10-04 14:02:58 +0700

    implement state->props in ContentNew.js

--- src/admin/pages/ContentNew.js
+++ src/admin/pages/ContentNew.js
@@ -86,7 +86,7 @@ let ContentNew = React.createClass({
       function(error, response, body) {
         if (!error && !body.errors && response.statusCode === 200) {
           var slugCount = body.data.viewer.allContents.edges.length;
-          if (me.state.mode==="create") {
+          if (me.props.mode==="create") {
             if (slugCount > 0) { 
             	// me.setState({checkingSlug: false, slug: slug+"-"+slugCount});
             	me.props.dispatch(togglecheckingSlug(false, slug+"-"+slugCount)); 
@@ -149,8 +149,8 @@ let ContentNew = React.createClass({
 		event.preventDefault();
 		var me = this;
 		var _objData = getFormData('rdt-input-form', 'object');
-		_objData['fields'] = this.state.providedFields;
-		_objData['customFields'] = this.state.customFields;
+		_objData['fields'] = this.props.providedFields;
+		_objData['customFields'] = this.props.customFields;
 
 		var status = "inactive";
 		var statusEl = document.getElementById("status");
@@ -160,7 +160,7 @@ let ContentNew = React.createClass({
 		this.disableForm(true);
 
 		var qry = "";
-	    if (this.state.mode==="create"){
+	    if (this.props.mode==="create"){
 	      qry = Query.createContentMtn(_objData);
 	    }else{
 	      _objData["id"] = this.props.postId;
@@ -193,11 +193,11 @@ let ContentNew = React.createClass({
     })
 		// this.setState({providedFields: providedFields, fields: _.concat(providedFields, this.state.customFields)});
 		this.props.dispatch(setprovidedFields(providedFields));
-		this.props.dispatch(setfields(_.concat(providedFields, this.state.customFields)));
+		this.props.dispatch(setfields(_.concat(providedFields, this.props.customFields)));
 	},
 	handleAddCustomField: function(event){
 		event.preventDefault();
-		var customFields = this.state.customFields;
+		var customFields = this.props.customFields;
 		var name = getValue("field-name");
 		var type = getValue("field-type");
 		var width = getValue("field-width");
@@ -225,13 +225,13 @@ let ContentNew = React.createClass({
 		customFields.push(newField);
 		// this.setState({customFields: customFields, fields: _.concat(this.state.providedFields, customFields)});
 		this.props.dispatch(setcustomFields(customFields));
-		this.props.dispatch(setfields(_.concat(this.state.providedFields, customFields)))
+		this.props.dispatch(setfields(_.concat(this.props.providedFields, customFields)))
 	},
 	handleFieldDelete: function(event){
 		event.preventDefault();
 		var name = event.target.getAttribute("data");
-		var cfields = this.state.customFields;
-		var pfields = this.state.providedFields;
+		var cfields = this.props.customFields;
+		var pfields = this.props.providedFields;
 		
 		var record = _.find(cfields, {label: name});
 		if (record) {
@@ -275,8 +275,8 @@ let ContentNew = React.createClass({
 			<div className="content-wrapper">
 				<div className="container-fluid">
 				<section className="content-header">
-			      <h1>{this.state.mode==="update"?"Edit Content Type":"Add New Content Type"}
-              { this.state.mode==="update" &&
+			      <h1>{this.props.mode==="update"?"Edit Content Type":"Add New Content Type"}
+              { this.props.mode==="update" &&
                 <small style={{marginLeft: 5}}>
                   <button className="btn btn-default btn-primary add-new-post-btn" onClick={this.handleAddNewBtn}>Add new</button>
                 </small>
@@ -302,7 +302,7 @@ let ContentNew = React.createClass({
 		            </div>
 			    			<div className="box-body">
 
-			    			{ this.state.mode === "update" &&
+			    			{ this.props.mode === "update" &&
 			    			<div className="form-group">
 			          	<label htmlFor="fields" className="col-md-3">Content type status</label>
 							  	<div className="col-md-9">
@@ -324,7 +324,7 @@ let ContentNew = React.createClass({
 							  	<div className="col-md-9">
 							  		<div className="form-inline">
 							  			<Field name="slug" component="input" type="text" className="form-control" onBlur={this.handleNameBlur} />
-											{ this.state.checkingSlug && <i style={{marginLeft:5}} className="fa fa-spin fa-refresh"></i>}
+											{ this.props.checkingSlug && <i style={{marginLeft:5}} className="fa fa-spin fa-refresh"></i>}
 											<p className="help-block">ID for the custom content type ( max. 20 characters ). Alphanumeric lower-case characters and underscores only. Min 2 letters. Once added the post type system name cannot be changed.</p>
 										</div>
 									</div>
@@ -453,7 +453,7 @@ let ContentNew = React.createClass({
 										</div>
 										<h4>Current fields</h4>
 										{
-											this.state.fields.map(function(item){
+											this.props.fields.map(function(item){
 												return <ContentField 
 																key={item.label}
 																name={item.label} 
@@ -474,28 +474,28 @@ let ContentNew = React.createClass({
 		            	<div className="form-group">
 									 	<label htmlFor="label" className="col-md-3">Name</label>
 									 	<div className="col-md-9">
-											<Field name="label" component="input" type="text" placeholder={this.state.label} className="form-control" style={{width: 'auto'}} />
+											<Field name="label" component="input" type="text" placeholder={this.props.label} className="form-control" style={{width: 'auto'}} />
 										</div>
 									</div>
 
 									<div className="form-group">
 									 	<label htmlFor="label-singular" className="col-md-3">Singular Name</label>
 									 	<div className="col-md-9">
-											<Field name="labelSingular" component="input" type="text" placeholder={this.state.labelSingular} className="form-control" style={{width: 'auto'}} />
+											<Field name="labelSingular" component="input" type="text" placeholder={this.props.labelSingular} className="form-control" style={{width: 'auto'}} />
 										</div>
 									</div>
 
 									<div className="form-group">
 									 	<label htmlFor="label-add-new" className="col-md-3">Add New</label>
 									 	<div className="col-md-9">
-											<Field name="labelAddNew" component="input" type="text" placeholder={this.state.labelAddNew} className="form-control" style={{width: 'auto'}} />
+											<Field name="labelAddNew" component="input" type="text" placeholder={this.props.labelAddNew} className="form-control" style={{width: 'auto'}} />
 										</div>
 									</div>
 
 									<div className="form-group">
 									 	<label htmlFor="label-edit" className="col-md-3">Edit</label>
 									 	<div className="col-md-9">
-											<Field name="labelEdit" component="input" type="text" placeholder={this.state.labelEdit} className="form-control" style={{width: 'auto'}} />
+											<Field name="labelEdit" component="input" type="text" placeholder={this.props.labelEdit} className="form-control" style={{width: 'auto'}} />
 										</div>
 									</div>
 		            </div>
@@ -503,7 +503,7 @@ let ContentNew = React.createClass({
 								<div className="form-group">
 									<div className="col-md-9">
 										<div className="btn-group">
-											<input type="submit" value={this.state.mode==="update"?"Update":"Add"} className="btn btn-primary btn-sm" />
+											<input type="submit" value={this.props.mode==="update"?"Update":"Add"} className="btn btn-primary btn-sm" />
 										</div>
 									</div>
 								</div>
