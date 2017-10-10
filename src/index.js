6fb9147b534fdc2e9fac9d1cf622a60c0284ba52
ihfazhillah <mihfazhillah@gmail.com>
2017-10-10 17:23:10 +0700

    refactor themeSingle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {ApolloProvider} from 'react-apollo'
-import BrowserRouter from 'react-router/BrowserRouter'
-import Match from 'react-router/Match'
-import Miss from 'react-router/Miss'
-import MatchWhenAuthorized from './auth'
 import client from './apollo'
 import reducer from './reducers'
 import DragDropContext from 'react-dnd/lib/DragDropContext';
@@ -13,84 +9,10 @@ import createStore from 'redux/lib/createStore'
 import Loading from './admin/Loading';
 import Loadable from 'react-loadable';
 
+let Main = Loadable({
+  loader: () => import(/* webpackChunkName: "app" */'./main'),
+  loading: () => <Loading/>
+})
 const store = createStore(reducer, {})
-
-let Main = React.createClass({
-	render(){
-		return (
-				<BrowserRouter>
-					<div id="router" style={{height: "100vh"}}>
-							<MatchWhenAuthorized pattern="/admin/:page?/:action?/:postId?" 
-                component={props => {
-              const Admin = Loadable({
-                loader: () =>import(/* webpackChunkName: "admin"*/'./admin'),
-                loading: () => <Loading/>
-              })
-                  return <Admin {...props}/>
-                }}
-								/>
-              <Match pattern="/page/:postId?/:param1?/:param2?" render={props=>{
-                  const ThemeSingle = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "ThemeSingle"*/ './includes/Theme/ThemeSingle'),
-                    loading: () => null
-                  })
-                return <ThemeSingle {...props} />             }} />
-              <Match pattern="/post/:postId?/:param1?/:param2?" render={props =>{
-                  const ThemeSingle = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "ThemeSingle"*/ './includes/Theme/ThemeSingle'),
-                    loading: () => null
-                  })
-                return <ThemeSingle {...props} />             }} />
-              <Match pattern="/blog/:postId?/:param1?/:param2?" render={props => {
-                  const ThemeBlog = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "ThemeBlog"*/ './includes/Theme/ThemeBlog'),
-                    loading: () => <Loading/>
-                  })
-                return <ThemeBlog {...props}/>
-              }} />
-            <Match pattern="/category/:categoryId?/:param1?/:param2?" render={props => {
-                  const ThemeBlog = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "ThemeBlog"*/ './includes/Theme/ThemeBlog'),
-                    loading: () => <Loading/>
-                  })
-                return <ThemeBlog {...props}/>
-            }} />
-          <Match pattern="/search/:search" render={props => {
-                  const ThemeSearch = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "Themesearch"*/ './includes/themeSearch'),
-                    loading: () => <Loading/>
-})
-            return <ThemeSearch {...props}/>
-          }}/>
-        <Match pattern="/register/:param1?" render={props => {
-                  const Register = Loadable({
-                    loader: () =>  import(/* webpackChunkName: "register"*/ './register'),
-                    loading: () => <Loading/>
-                  })
-
-          return <Register {...props}/>
-
-        }}/>
-              <Match pattern="/login/:param1?" render={props=> {
-                const Login = Loadable({
-                  loader: () =>  import(/* webpackChunkName: "login"*/ './login'),
-                  loading: () => <Loading/>
-                })
-                return <Login {...props}/>
-              }}/>
-            <Miss render={props => {
-                const ThemeHome = Loadable({
-                  loader: () => import(/* webpackChunkName: "themehome" */'./includes/Theme/ThemeHome'),
-                  loading: () => <Loading/>
-                })
-
-                return <ThemeHome {...props}/>
-                }} />
-					</div>
-				</BrowserRouter>
-		)
-	}
-})
-
 Main = DragDropContext(HTML5Backend)(Main);
 render(<ApolloProvider client={client} store={store}><Main/></ApolloProvider>, document.getElementById('root'));
