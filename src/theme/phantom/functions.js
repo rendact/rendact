50eca43d68c5e882c0261f30c9bf81aaf75bfa27
ihfazhillah <mihfazhillah@gmail.com>
2017-10-19 20:36:29 +0700

    paster functions.js

---
+++ src/theme/phantom/functions.js
@@ -0,0 +1,9 @@
+import {registerWidgetArea} from '../../includes/widgetUtils'
+
+const functions = function(){		
+	registerWidgetArea("Sidebar");
+	registerWidgetArea("Single");
+	registerWidgetArea("Footer");
+}
+
+export default functions
