b26d9ac2672b6766e83ebe57f4748e7f8418aa38
ihfazhillah <mihfazhillah@gmail.com>
2017-10-25 06:27:23 +0700

    add handling frontPage is page

--- addons/themes/massively/layouts/Home.js
+++ addons/themes/massively/layouts/Home.js
@@ -5,11 +5,53 @@ import Footer from '../includes/Footer';
 import PostList from '../includes/PostList';
 import NavPanel from '../includes/NavPanel';
 
+class HomePostList extends React.Component {
+  render(){
+    return (
+      <div>
+
+        <section className="posts">
+          {this.props.data && this.props.data.map(post => (
+            <PostList 
+              key={post.id}
+              imageFeatured={post.imageFeatured?post.imageFeatured.blobUrl:require('images/logo-128.png')}
+              content={post.content && post.content.slice(0, 100) + "..."}
+              postId={post.id}
+              date={post.publishDate && post.publishDate.toString()}
+              title={post.title}
+            />
+          ))}
+
+        </section>
+        <footer>
+          {this.props.thePagination}
+        </footer>
+      </div>
+    )
+  }
+}
+
+class HomePage extends React.Component {
+  render(){
+    return (
+      this.props.data ?
+      <section className="post">
+        <div className="image main"><img src={this.props.data.imageFeatured?this.props.data.imageFeatured.blobUrl:require('images/logo-128.png')} alt=""/></div>
+        <p dangerouslySetInnerHTML={{__html: this.props.data.content}}/>
+      </section>
+        :
+        null
+      
+    )
+  }
+}
+
 class Home extends React.Component {
   constructor(props){
     super(props)
 
     this.handleHideIntro = this.handleHideIntro.bind(this)
+    this.renderContent = this.renderContent.bind(this)
 
     this.state = {
       intro: true,
@@ -44,29 +86,22 @@ class Home extends React.Component {
 
   }
 
+  renderContent(){
+    return this.props.theConfig.frontPage === 'latestPost' ?
+            <HomePostList {...this.props}/>
+            :
+            <HomePage {...this.props}/>
+  }
+
 
   render(){
     return <div>
       <div className="fade-in" id="wrapper">
       <HeaderWrapper intro={this.state.intro} isHome={true} {...this.props}/>
       <div id="main">
-        <section className="posts">
-          {this.props.data && this.props.data.map(post => (
-            <PostList 
-              key={post.id}
-              imageFeatured={post.imageFeatured?post.imageFeatured.blobUrl:require('images/logo-128.png')}
-              content={post.content && post.content.slice(0, 100) + "..."}
-              postId={post.id}
-              date={post.publishDate && post.publishDate.toString()}
-              title={post.title}
-            />
-          ))}
-
-        </section>
-
-        <footer>
-          {this.props.thePagination}
-        </footer>
+        {this.props.loadDone ? this.renderContent() :
+            <div style={{height: "75%"}}>&nbsp;</div>
+        }
       </div>
 
 
