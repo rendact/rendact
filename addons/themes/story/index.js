1ae42f5d79f7f7d195812675a6641b047ff35b5e
mukhlis <mukhlisk93@gmail.com>
2017-10-27 10:45:35 +0700

    initially add story theme#2

--- addons/themes/story/index.js
+++ addons/themes/story/index.js
@@ -1,4 +1,1 @@
 export const Home = require ('./layouts/Home').default
-export const Single = require ('./layouts/Single').default
-export const Blog = require ('./layouts/Blog').default
-export const widgetArea = ["Sidebar", "Single", "Footer"]
