3f645ac510d082731e81b4b2222266a1d67ce0d9
ihfazhillah <mihfazhillah@gmail.com>
2017-10-31 17:20:57 +0700

    change implementation of PostList

--- addons/themes/stellar/includes/PostList.js
+++ addons/themes/stellar/includes/PostList.js
@@ -3,15 +3,26 @@ import { Link } from "react-router";
 
 class PostList extends React.Component {
   render() {
-    let { image, content, title, date } = this.props;
+    let { image, content, title, date, id } = this.props;
     return (
-      <div>
-        <span className="image left">
-          <img src={image} />
-        </span>
-        <h2>{title}</h2>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
+      <section className="row">
+        <div className="8u">
+          <h2 style={{ textTransform: "capitalize" }}>{title}</h2>
+          <p dangerouslySetInnerHTML={{ __html: content }} />
+          <ul className="actions align-right">
+            <li>
+              <Link to={"/post/" + id} className="button">
+                Read More
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="4u">
+          <span className="image fit">
+            <img src={image} />
+          </span>
+        </div>
+      </section>
     );
   }
 }
--- addons/themes/stellar/layouts/Home.js
+++ addons/themes/stellar/layouts/Home.js
@@ -4,17 +4,39 @@ import Footer from "../includes/Footer";
 import Main from "../includes/Main";
 import Content from "../includes/Content";
 import Nav from "../includes/Nav";
+import PostList from "../includes/PostList";
 
 class Home extends React.Component {
+  renderPostList(data, thePagination) {
+    return (
+      <section className="main">
+        {data &&
+          data.map(post => (
+            <PostList
+              key={post.id}
+              title={post.title}
+              content={post.content && post.content.trim().slice(0, 100)}
+              image={
+                post.imageFeatured
+                  ? post.imageFeatured.blobUrl
+                  : require("images/logo-128.png")
+              }
+              id={post.id}
+            />
+          ))}
+      </section>
+    );
+  }
+
   componentDidMount() {
     require("../assets/css/main.css");
   }
   render() {
     return (
       <div id="wrapper">
-        <Header />
+        <Header title="Rendact" tagline="hello" />
         <Nav />
-        <Content />
+        <Main>{this.renderPostList(this.props.data)}</Main>
         <Footer />
       </div>
     );
