fe0d7815658ecd65d5fead23c03a60d231c38850
ihfazhillah <mihfazhillah@gmail.com>
2017-11-01 10:40:35 +0700

    initally add Sidebar.js

---
+++ addons/themes/hyperspace/includes/Sidebar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <section id="sidebar">
+        <div className="inner">
+          <ul>
+            <li>
+              <a>1</a>
+            </li>
+            <li>
+              <a>2</a>
+            </li>
+            <li>
+              <a>3</a>
+            </li>
+            <li>
+              <a>4</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Sidebar;
