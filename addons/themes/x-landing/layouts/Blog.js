749959fd6201be3d6fcacc846fb11747b8f01297
ihfazhillah <mihfazhillah@gmail.com>
2017-11-01 09:41:29 +0700

    handle blog layout in landing

--- addons/themes/x-landing/layouts/Blog.js
+++ addons/themes/x-landing/layouts/Blog.js
@@ -1,40 +1,79 @@
-import React from 'react'
-import Header from '../includes/Header'
-import Sidebar from '../includes/Sidebar'
-import Footer from '../includes/Footer'
+import React from "react";
+import Header from "../includes/Header";
+import Footer from "../includes/Footer";
+import HeaderMobile from "../includes/HeaderMobile";
+import moment from "moment";
+import scrollToElement from "scroll-to-element";
+import { Link } from "react-router";
 
-const Blog = React.createClass({
-	render: function() {
-		return (
-			<div className="application">
-				<Header {...this.props} />   
-				<div className="container">
-					<div className="col-md-8 new-section">
-						     {
-						     	this.props.latestPosts && this.props.latestPosts.map(function(item){
-						     		return 	<div className="new">
-												<div className="col-md-6 new-text wow rollIn animated animated" data-wow-delay="0.4s" style={{"visibility": "visible", "animationDelay": "0.4s", "animationName": "rollIn"}}>
-													<small>{item.createdAt}</small>
-													{this.props.theTitle(item.id, item.title)}
-													<section className="content-body">
-														{this.props.theContent(item.content)}
-													</section>
-												</div>
-												<div className="col-md-6 welcome-img">
-													<a href="article" className="mask"><img src={item.featuredImage} alt="" className="img-responsive zoom-img" /></a>
-												</div>
-												<div className="clearfix"> </div>
-											</div>
-						     	}.bind(this))
-						     }
-					</div>	
-					<Sidebar {...this.props} />
-				  <div className="clearfix"></div>
-				</div>
-				<Footer {...this.props} />	
-			</div>
-		)
-	}
+const HomeContentWithLatestPost = props => (
+  <div>
+    {props.latestPosts ? (
+      props.latestPosts.map(item => {
+        return (
+          <div key={item.id} className="new">
+            <div className="col-md-6 new-text wow fadeIn animated">
+              {props.theTitle(item.id, item.title)}
+              <small>
+                {moment(item.createdAt).format("MMMM Do YY, h:mm:ss a")}
+              </small>
+              <section className="content-body">
+                {props.theExcerpt(item.content)}
+              </section>
+            </div>
+            <div className="col-md-6 welcome-img">
+              {props.theImage && props.theImage(item.imageFeatured)}
+            </div>
+            <div className="clearfix"> </div>
+          </div>
+        );
+      })
+    ) : (
+      <div style={{ height: 760, width: "100%", clear: "both" }}>&nbsp;</div>
+    )}
+    {props.thePagination}
+  </div>
+);
+
+const Home = React.createClass({
+  onContactLinkClick: function(e) {
+    if (
+      e.target &&
+      e.target.nodeName === "A" &&
+      e.target.className === "button"
+    ) {
+      e.preventDefault();
+      this._reactInternalInstance._context.history.push("/page/UG9zdDozNzQ=");
+    }
+  },
+
+  onGotoScrollyClick: function(e) {
+    if (
+      e.target &&
+      e.target.nodeName === "A" &&
+      e.target.className === "goto-next scrolly"
+    ) {
+      e.preventDefault();
+      scrollToElement("#items", { duration: 2000, offset: 0 });
+    }
+  },
+
+  componentDidMount: function() {
+    document.body.addEventListener("click", this.onContactLinkClick);
+    document.body.addEventListener("click", this.onGotoScrollyClick);
+    require("../css/style.css");
+    require("../css/main.min.css");
+  },
+  render: function() {
+    return (
+      <div id="page-wrapper">
+        <Header {...this.props} />
+        {<HomeContentWithLatestPost {...this.props} />}
+        <Footer {...this.props} />
+        <HeaderMobile {...this.props} />
+      </div>
+    );
+  }
 });
 
-export default Blog
+export default Home;
