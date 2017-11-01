ef9cd690e43687da5f94e74e8e30ef2de87efd42
ihfazhillah <mihfazhillah@gmail.com>
2017-11-01 19:41:49 +0700

    Change the blog layout

--- addons/themes/hyperspace/layouts/Blog.js
+++ addons/themes/hyperspace/layouts/Blog.js
@@ -1,8 +1,52 @@
 import React from "react";
+import Nav from "../includes/Nav";
+import Wrapper from "../includes/Wrapper";
+import Sidebar from "../includes/Sidebar";
+import Footer from "../includes/Footer";
+import Spotlight from "../includes/Spotlight";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderPostList = this.renderPostList.bind(this);
+  }
+
+  componentDidMount() {
+    require("../assets/css/main.css");
+  }
+
+  renderPostList(data, pagination) {
+    return (
+      <section className="wrapper style2 spotlights">
+        {data &&
+          data.map((post, idx) => (
+            <Spotlight
+              className="inactive"
+              content={data.content && data.content.trim.slice(0, 100)}
+              title={post.title}
+              image={
+                post.imageFeatured
+                  ? post.imageFeatured.blobUrl
+                  : require("images/logo-128.png")
+              }
+              id={post.id}
+              key={post.id}
+            />
+          ))}
+      </section>
+    );
+  }
+
   render() {
-    return <p>Hello world</p>;
+    let { loadDone, theConfig, data } = this.props;
+    return (
+      <div>
+        <Nav {...this.props} />
+        <Wrapper>{this.renderPostList(data)}</Wrapper>
+        <Footer />
+      </div>
+    );
   }
 }
 
