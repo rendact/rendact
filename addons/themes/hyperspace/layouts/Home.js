a2f9d4917330edb54d2c44095a5b4cd2403699e3
ihfazhillah <mihfazhillah@gmail.com>
2017-11-01 16:48:49 +0700

    initial home layout

--- addons/themes/hyperspace/layouts/Home.js
+++ addons/themes/hyperspace/layouts/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Nav from "../includes/Nav";
+import Wrapper from "../includes/Wrapper";
+import Sidebar from "../includes/Sidebar";
+import Footer from "../includes/Footer";
+import Spotlight from "../includes/Spotlight";
 
 class Home extends React.Component {
   render() {
-    return <p>Hello world</p>;
+    return (
+      <div>
+        <Sidebar />
+        <Wrapper />
+        <Footer />
+      </div>
+    );
   }
 }
 
