505f280b1c5c6a857ef102daf47ff14cb90ad54a
ihfazhillah <mihfazhillah@gmail.com>
2017-11-06 14:51:55 +0700

    add script loader style loader

--- src/includes/Theme/includes.js
+++ src/includes/Theme/includes.js
@@ -1,31 +1,70 @@
-import React from 'react';
-import _ from 'lodash';
-import map from 'lodash/map'
-import get from 'lodash/get'
-import truncate from 'lodash/truncate'
-import {Menu} from '../Menu.js';
-import AdminConfig from '../../admin/AdminConfig';
-import {Link} from 'react-router'
-import Loadable from 'react-loadable';
-import CommentForm from './CommentForm';
-import {registerWidgetArea} from '../widgetUtils';
-import request from 'request';
-const vm = require('vm');
+import React from "react";
+import _ from "lodash";
+import map from "lodash/map";
+import get from "lodash/get";
+import truncate from "lodash/truncate";
+import { Menu } from "../Menu.js";
+import AdminConfig from "../../admin/AdminConfig";
+import { Link } from "react-router";
+import Loadable from "react-loadable";
+import CommentForm from "./CommentForm";
+import { registerWidgetArea } from "../widgetUtils";
+import request from "request";
+const vm = require("vm");
 
 const InvalidTheme = Loadable({
-  loader: () => import(/* webpackChunkName: "invalidTheme"*/'./InvalidTheme'),
+  loader: () => import(/* webpackChunkName: "invalidTheme"*/ "./InvalidTheme"),
   loading: () => null
-})
+});
+
+const loadScript = (host, themeName) => {
+  return new Promise((resolve, reject) => {
+    if (document.getElementById("themeScript")) {
+      return resolve();
+    }
+    let script;
+    script = document.createElement("script");
+    script.id = "themeScript";
+    script.src = host + "/" + themeName + "/" + themeName + ".js";
+    script.onload = resolve;
+    script.onerror = reject;
+    document.getElementById("theme").appendChild(script);
+  });
+};
+
+const loadStyle = (host, themeName) => {
+  return new Promise((resolve, reject) => {
+    if (document.getElementById("themeStyle")) {
+      return resolve();
+    }
+    let style;
+    style = document.createElement("link");
+    style.id = "themeStyle";
+    style.href = host + "/" + themeName + "/" + themeName + ".css";
+    style.type = "text/css";
+    style.onload = resolve;
+    style.onerror = reject;
+    document.getElementById("theme").appendChild(style);
+  });
+};
 
 window.config = AdminConfig;
 
 /* Theme functions */
 
-export function getTemplateComponent(type){
-	var c = JSON.parse(
-		JSON.parse(localStorage.getItem("config")).activeTheme
-	)
-	/*
+export function getTemplateComponent(type) {
+  loadScript(
+    "https://59ffa3d3a6188f1d60f856af--shopkeeper-lionel-47443.netlify.com",
+    "stellar"
+  ).then(() =>
+    loadStyle(
+      "https://59ffa3d3a6188f1d60f856af--shopkeeper-lionel-47443.netlify.com",
+      "stellar"
+    ).then(() => console.log("hello"))
+  );
+
+  var c = JSON.parse(JSON.parse(localStorage.getItem("config")).activeTheme);
+  /*
 	request({
 		url: "http://astrologer-forehead-75301.netlify.com/themes/default/index.js",
 		method: "GET",
@@ -42,17 +81,17 @@ export function getTemplateComponent(type){
 			//var widgetAreas = require("./remote/theme")["widgetArea"];		
 	})
 	*/
-	
+
   //	var widgetAreas = require("themes/"+c.path)["widgetArea"];
   import(`themes/${c.path}`).then(theme => {
     let widgetAreas = theme["widgetArea"];
     if (widgetAreas) {
       widgetAreas.forEach(widgetId => {
-        registerWidgetArea(widgetId)
-      })
+        registerWidgetArea(widgetId);
+      });
     }
-  })
-    /*
+  });
+  /*
 	if (widgetAreas) {
 		_.forEach(widgetAreas, function(widgetId){
 			registerWidgetArea(widgetId)
@@ -61,129 +100,186 @@ export function getTemplateComponent(type){
   */
 
   const themeMap = {
-    home: 'Home',
-    blog: 'Blog',
-    single: 'Single',
-    search: 'Search'
+    home: "Home",
+    blog: "Blog",
+    single: "Single",
+    search: "Search"
+  };
+
+  if (c.name == null || c.path == null) {
+    return InvalidTheme;
   }
-	
-	if (c.name==null || c.path==null) {
-		return InvalidTheme;
-	}
   let module = themeMap[type];
   return import(`themes/${c.path}`).then(theme => {
-    if(theme){
-      return theme[module]
-    } return null
-  })
+    if (theme) {
+      return theme[module];
+    }
+    return null;
+  });
   //return require("themes/"+c.path)[module]
 }
 
-export function theContent(content){
-	return <div dangerouslySetInnerHTML={{__html: content}} />
+export function theContent(content) {
+  return <div dangerouslySetInnerHTML={{ __html: content }} />;
 }
 
-export function theTitle(id, title, onClickHandler){
-	return <Link to={"/post/"+id} onClick={onClickHandler} id={id}><h4>{title}</h4></Link>
+export function theTitle(id, title, onClickHandler) {
+  return (
+    <Link to={"/post/" + id} onClick={onClickHandler} id={id}>
+      <h4>{title}</h4>
+    </Link>
+  );
 }
 
-export function theExcerpt(content){
-	return <div dangerouslySetInnerHTML={{__html: truncate(content,{"length": 100})}} />
+export function theExcerpt(content) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: truncate(content, { length: 100 }) }}
+    />
+  );
 }
 
-export function theMenu(items, onClickHandler){
-  return (ulParentClass) => (
-    <Menu menuItems={items&&items.items?items.items:[]} goHome={goHome} ulParentClass={ulParentClass}/>
-  )
+export function theMenu(items, onClickHandler) {
+  return ulParentClass => (
+    <Menu
+      menuItems={items && items.items ? items.items : []}
+      goHome={goHome}
+      ulParentClass={ulParentClass}
+    />
+  );
 }
 
-export function theLogo(onClickHandler){
-	return <div className="logo">
-		<Link to="/"><h1>Rend<span>act</span></h1></Link>
-	</div>
+export function theLogo(onClickHandler) {
+  return (
+    <div className="logo">
+      <Link to="/">
+        <h1>
+          Rend<span>act</span>
+        </h1>
+      </Link>
+    </div>
+  );
 }
 
-export function theImage(image, pageCount, activePage, handlePageClick){
-	var fImage="";
-	var c = JSON.parse(
-		JSON.parse(localStorage.getItem("config")).activeTheme
-	)
-	if(image!=null){
- 			fImage=image.blobUrl;
- 		}
- 		else{
- 			try {
-			 fImage=require('themes/'+c.path+'/images/logo.png');
-			}
-			catch (e) {
-			 fImage=require('images/logo-128.png');
-			}
- 		}
-	return <a href="article" className="mask"><img src={fImage} alt="" className="img-responsive img-thumbnail" /></a>
+export function theImage(image, pageCount, activePage, handlePageClick) {
+  var fImage = "";
+  var c = JSON.parse(JSON.parse(localStorage.getItem("config")).activeTheme);
+  if (image != null) {
+    fImage = image.blobUrl;
+  } else {
+    try {
+      fImage = require("themes/" + c.path + "/images/logo.png");
+    } catch (e) {
+      fImage = require("images/logo-128.png");
+    }
+  }
+  return (
+    <a href="article" className="mask">
+      <img src={fImage} alt="" className="img-responsive img-thumbnail" />
+    </a>
+  );
 }
 
-export function thePagination(pageCount, activePage, handlePageClick){
-	let pages = [<li key="998" ><a href="#" onClick={handlePageClick}>«</a></li>];
-	for(var i=0;i<pageCount;i++){
-		if (activePage===i+1)
-			pages.push(<li key={i}><a href="#" onClick={handlePageClick} disabled="true">{i+1}</a></li>)
-		else 
-			pages.push(<li key={i}><a href="#" onClick={handlePageClick}>{i+1}</a></li>)
-	}
-	pages.push(<li key="999"><a href="#" onClick={handlePageClick}>»</a></li>);
-	return <div className="box-tools">
-          <ul className="pagination pagination-sm no-margin">
-          {pages}  
-          </ul>
-        </div>
+export function thePagination(pageCount, activePage, handlePageClick) {
+  let pages = [
+    <li key="998">
+      <a href="#" onClick={handlePageClick}>
+        «
+      </a>
+    </li>
+  ];
+  for (var i = 0; i < pageCount; i++) {
+    if (activePage === i + 1)
+      pages.push(
+        <li key={i}>
+          <a href="#" onClick={handlePageClick} disabled="true">
+            {i + 1}
+          </a>
+        </li>
+      );
+    else
+      pages.push(
+        <li key={i}>
+          <a href="#" onClick={handlePageClick}>
+            {i + 1}
+          </a>
+        </li>
+      );
+  }
+  pages.push(
+    <li key="999">
+      <a href="#" onClick={handlePageClick}>
+        »
+      </a>
+    </li>
+  );
+  return (
+    <div className="box-tools">
+      <ul className="pagination pagination-sm no-margin">{pages}</ul>
+    </div>
+  );
 }
 
-export function theBreadcrumb(onClickHandler){
-	return <h2><a href="#" onClick={onClickHandler}><h5>Home </h5></a> / PAGE</h2>
+export function theBreadcrumb(onClickHandler) {
+  return (
+    <h2>
+      <a href="#" onClick={onClickHandler}>
+        <h5>Home </h5>
+      </a>{" "}
+      / PAGE
+    </h2>
+  );
 }
 
-export function goHome(e){
-	e.preventDefault();
-	this._reactInternalInstance._context.history.push('/')
+export function goHome(e) {
+  e.preventDefault();
+  this._reactInternalInstance._context.history.push("/");
 }
 
-export function getWidgets(widgetArea){
+export function getWidgets(widgetArea) {
   // add checking if the component has implemented with redux or not
-  let listOfWidgets = this.props.listOfWidgets[widgetArea]?this.props.listOfWidgets[widgetArea]:[];
-	
-	let Widgets = map(listOfWidgets,function(item){
-    return import("../DefaultWidgets/"+item.filePath).then(widgetFn => {
-		
-    return <div key={item.id} className="sidebar-box">
-            <h3><span>{item.title}</span></h3>
-              {widgetFn.default(item.id, item.widget)}
-          </div>
-  })
-	});
+  let listOfWidgets = this.props.listOfWidgets[widgetArea]
+    ? this.props.listOfWidgets[widgetArea]
+    : [];
 
-  return Promise.all(Widgets).then(Widgets => <div className="widgets">{Widgets}</div>)
-	
+  let Widgets = map(listOfWidgets, function(item) {
+    return import("../DefaultWidgets/" + item.filePath).then(widgetFn => {
+      return (
+        <div key={item.id} className="sidebar-box">
+          <h3>
+            <span>{item.title}</span>
+          </h3>
+          {widgetFn.default(item.id, item.widget)}
+        </div>
+      );
+    });
+  });
+
+  return Promise.all(Widgets).then(Widgets => (
+    <div className="widgets">{Widgets}</div>
+  ));
 }
 
-export const getTemplates = function(){
-	var template = [{
-			id: "default",
-			name: "Default Template"
-		}];
-	//var c = window.config.theme;
-	try {
-		//let Component = require('../theme/'+c.path+'/layouts/Template.js').default;
-		template = [{
-			id: "default",
-			name: "Default Template"
-		}]
-	} catch(e) {
-		
-	}
-	return template;
-}
+export const getTemplates = function() {
+  var template = [
+    {
+      id: "default",
+      name: "Default Template"
+    }
+  ];
+  //var c = window.config.theme;
+  try {
+    //let Component = require('../theme/'+c.path+'/layouts/Template.js').default;
+    template = [
+      {
+        id: "default",
+        name: "Default Template"
+      }
+    ];
+  } catch (e) {}
+  return template;
+};
 
-export const theCommentForm = function(postId){
-	return <CommentForm postId={postId} />
-}
-
+export const theCommentForm = function(postId) {
+  return <CommentForm postId={postId} />;
+};
