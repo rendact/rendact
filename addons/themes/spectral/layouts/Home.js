db2ce6d6e9e659610c6fbb582fa5c482c97a84fe
mukhlis <mukhlisk93@gmail.com>
2017-11-16 10:51:04 +0700

    add pagination on Spectral theme

--- addons/themes/spectral/layouts/Home.js
+++ addons/themes/spectral/layouts/Home.js
@@ -91,37 +91,9 @@ let Home = React.createClass({
 
           <section id="three" className="wrapper style3 special">
             <div className="inner">
-              <header className="major">
-                <h2>Accumsan mus tortor nunc aliquet</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
-                fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
-              </header>
-              <ul className="features">
-                <li className="icon fa-paper-plane-o">
-                  <h3>Arcu accumsan</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-                <li className="icon fa-laptop">
-                  <h3>Ac Augue Eget</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-                <li className="icon fa-code">
-                  <h3>Mus Scelerisque</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-                <li className="icon fa-headphones">
-                  <h3>Mauris Imperdiet</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-                <li className="icon fa-heart-o">
-                  <h3>Aenean Primis</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-                <li className="icon fa-flag-o">
-                  <h3>Tortor Ut</h3>
-                  <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
-                </li>
-              </ul>
+              <div style={{textAlign: "center"}}>
+                {thePagination}
+              </div>
             </div>
           </section>
 
