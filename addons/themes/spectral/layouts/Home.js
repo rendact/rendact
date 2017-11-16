2d5a500c66c1af1b1dfc2820f19230be63e001c9
mukhlis <mukhlisk93@gmail.com>
2017-11-16 10:31:54 +0700

    show posts on the body of Spectral theme

--- addons/themes/spectral/layouts/Home.js
+++ addons/themes/spectral/layouts/Home.js
@@ -72,27 +72,21 @@ let Home = React.createClass({
           </section>
 
           <section id="two" className="wrapper alt style2">
-            <section className="spotlight">
-              <div className="image"><img src="images/pic01.jpg" alt="" /></div><div className="content">
-                <h2>Magna primis lobortis<br />
-                sed ullamcorper</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
-              </div>
-            </section>
-            <section className="spotlight">
-              <div className="image"><img src="images/pic02.jpg" alt="" /></div><div className="content">
-                <h2>Tortor dolore feugiat<br />
-                elementum magna</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
-              </div>
-            </section>
-            <section className="spotlight">
-              <div className="image"><img src="images/pic03.jpg" alt="" /></div><div className="content">
-                <h2>Augue eleifend aliquet<br />
-                sed condimentum</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
-              </div>
-            </section>
+
+            {data && data.map((post, index) => (
+              <section className="spotlight">
+                <div className="image">
+                  <Link to={"/post/" + post.id}>
+                    <img src={post.imageFeatured ? post.imageFeatured.blobUrl: require('images/logo-128.png') } alt="" />
+                  </Link></div>
+                <div className="content">
+                  <h2>{post.title && post.title}</h2>
+                  <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                  <Link className="button" to={"/post/" + post.id}>Read More</Link>
+                </div>
+              </section>
+            ))}
+
           </section>
 
           <section id="three" className="wrapper style3 special">
