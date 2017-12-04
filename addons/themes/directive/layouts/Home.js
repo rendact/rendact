bd6a1be84bbfdd1e560cd1daca207d4b55acf6d4
mukhlis <mukhlisk93@gmail.com>
2017-12-04 15:53:32 +0700

    set Home.js og Directive theme

--- addons/themes/directive/layouts/Home.js
+++ addons/themes/directive/layouts/Home.js
@@ -22,93 +22,58 @@ let Home = React.createClass({
     return (
     <div>
       <div id="header">
-        <span className="logo icon fa-paper-plane-o"></span>
-        <h1>Hi. This is Directive.</h1>
-        <p>A responsive HTML5 + CSS3 site template designed by <a href="http://html5up.net">HTML5 UP</a>
-        <br />
-        and released for free under the <a href="http://html5up.net/license">Creative Commons license</a>.</p>
+        <span className="logo icon">
+          <Link to={"/"}>
+            <img src={ require('images/logo-circle.svg') } alt="" />
+          </Link>
+        </span>
+        <h1>{theConfig?theConfig.name:"Rendact"}</h1>
+        <p>{theConfig?theConfig.tagline:"Hello, you are in Rendact"}</p>
+        <nav id="menu">
+          {this.props.theMenu()}
+        </nav>
       </div>
 
       <div id="main">
 
-        <header className="major container 75%">
-          <h2>We conduct experiments that
-          <br />
-          may or may not seriously
-          <br />
-          break the universe</h2>
-          
-          <p>Tellus erat mauris ipsum fermentum<br />
-          etiam vivamus nunc nibh morbi.</p>
-          
+        <header className="major container 90%">
         </header>
 
         <div className="box alt container">
-          <section className="feature left">
-            <a href="#" className="image icon fa-signal"><img src="images/pic01.jpg" alt="" /></a>
-            <div className="content">
-              <h3>The First Thing</h3>
-              <p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus eget. Nunc nibh morbi quis fusce lacus.</p>
-            </div>
-          </section>
-          <section className="feature right">
-            <a href="#" className="image icon fa-code"><img src="images/pic02.jpg" alt="" /></a>
-            <div className="content">
-              <h3>The Second Thing</h3>
-              <p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus eget. Nunc nibh morbi quis fusce lacus.</p>
-            </div>
-          </section>
-          <section className="feature left">
-            <a href="#" className="image icon fa-mobile"><img src="images/pic03.jpg" alt="" /></a>
-            <div className="content">
-              <h3>The Third Thing</h3>
-              <p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus eget. Nunc nibh morbi quis fusce lacus.</p>
-            </div>
-          </section>
+          {data && data.map((post, index) => (
+            <section className={index%2===0 ?"feature left" : "feature right"}>
+              <Link className="image icon" to={"/post/" + post.id}>
+                <img src={post.imageFeatured ? post.imageFeatured.blobUrl: require('images/logo-128.png') } alt="" />
+              </Link>
+              <div className="content">
+                <h3>
+                  <Link to={"/post/" + post.id}>{post.title && post.title}</Link>
+                </h3>
+                <p dangerouslySetInnerHTML={{__html: post.content ? post.content.slice(0, 160):""}} />
+                <ul className="actions">
+                  <li>
+                    <Link className="button" to={"/post/" + post.id}>Read More</Link>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          ))}
         </div>
 
-        <footer className="major container 75%">
-          <h3>Get shady with science</h3>
-          <p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus.</p>
-          <ul className="actions">
-            <li><a href="#" className="button">Join our crew</a></li>
-          </ul>
+        <footer className="major container 90%">
+          <div style={{textAlign: "center"}}>
+            {this.props.thePagination}
+          </div>
         </footer>
 
       </div>
 
       <div id="footer">
-        <div className="container 75%">
+        <div className="container 100%">
 
-          <header className="major last">
-            <h2>Questions or comments?</h2>
-          </header>
-
-          <p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor
-          orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus.</p>
-
-          <form method="post" action="#">
-            <div className="row">
-              <div className="6u 12u(mobilep)">
-                <input type="text" name="name" placeholder="Name" />
-              </div>
-              <div className="6u 12u(mobilep)">
-                <input type="email" name="email" placeholder="Email" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="12u">
-                <textarea name="message" placeholder="Message" rows="6"></textarea>
-              </div>
-            </div>
-            <div className="row">
-              <div className="12u">
-                <ul className="actions">
-                  <li><input type="submit" value="Send Message" /></li>
-                </ul>
-              </div>
-            </div>
-          </form>
+          <div className="row">
+            {this.props.footerWidgets && this.props.footerWidgets.map((fw, idx) => <div className="4u">{fw}</div>)}
+          </div>
 
           <ul className="icons">
             <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
@@ -119,7 +84,7 @@ let Home = React.createClass({
           </ul>
 
           <ul className="copyright">
-            <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>&copy; Rendact Team. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
           </ul>
 
         </div>
