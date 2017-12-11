815a3c98e909a48903e0c0cb22e0298259293a07
mukhlis <mukhlisk93@gmail.com>
2017-12-11 08:51:30 +0700

    remove fractal and forty theme

--- addons/themes/forty/index.js
+++
@@ -1,4 +0,0 @@
-export const Home = require ('./layouts/Home').default
-export const Single = require ('./layouts/Single').default
-export const Blog = require ('./layouts/Blog').default
-export const widgetArea = ["Sidebar", "Single", "Footer"]
