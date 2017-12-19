469b8175af0857eeba283e4a86f1482099aa1233
mukhlis <mukhlisk93@gmail.com>
2017-12-19 10:08:20 +0700

    add Strongly Typed theme

---
+++ addons/themes/strongly-typed/layouts/Home.js
@@ -0,0 +1,102 @@
+import $ from 'jquery'
+import React from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import moment from 'moment';
+import {Link} from 'react-router';
+import scrollToElement from 'scroll-to-element';
+
+let Home = React.createClass({
+  componentDidMount(){
+    require('../assets/css/main.css')
+  },
+
+  componentWillMount(){
+    document.body.className = "homepage";
+  },
+
+  handleScrolly(e){
+    scrollToElement("#main", {
+      duration: 1500,
+      offset: 0,
+      ease: 'in-sine'
+    })
+  },
+
+  render(){
+    let {
+      theConfig,
+      data,
+      thePagination,
+      loadDone
+    } = this.props
+
+    return (
+      <div>
+        <div id="page-wrapper">
+
+          <div id="header-wrapper">
+            <div id="header" className="container">
+
+                <h1 id="logo"><Link to={"/"}>{theConfig?theConfig.name:"Rendact"}</Link></h1>
+                <p>{theConfig?theConfig.tagline:"Hello"}</p>
+
+                <nav id="nav">
+                  {this.props.theMenu()}
+                </nav>
+
+            </div>
+          </div>
+
+          <div id="features-wrapper">
+            <section id="features" className="container">
+              <header>
+                <h2>Gentlemen, behold! This is <strong>{theConfig?theConfig.name:"Rendact"}</strong>!</h2>
+              </header>
+              <div className="row">
+                {data && data.map((post, index) => (
+                  <div className="6u 12u(mobile)">
+                      <section>
+                        <Link className="image featured" to={"/post/" + post.id}>
+                          <img src={post.imageFeatured ? post.imageFeatured.blobUrl: require('images/logo-128.png') } alt="" />
+                        </Link>
+                        <header>
+                          <h3><Link to={"/post/" + post.id}>{post.title && post.title}</Link></h3>
+                        </header>
+                        <p dangerouslySetInnerHTML={{__html: post.content ? post.content.slice(0, 160):""}} />
+                        <ul className="actions">
+                          <li><a href="#" className="button icon fa-file">Tell Me More</a></li>
+                        </ul>
+                      </section>
+                  </div>
+                ))}
+              </div>
+            </section>
+            <div className="image featured" style={{textAlign: "center"}}>
+              {this.props.thePagination}
+            </div>
+          </div>
+
+          
+
+          <div id="footer-wrapper">
+            <div id="footer" className="container">
+              <div className="row">
+                {this.props.footerWidgets &&
+                  this.props.footerWidgets.map((fw, idx) => <div className="4u 12u(mobile)">{fw}</div>)}
+              </div>
+            </div>
+            <div id="copyright" className="container">
+              <ul className="links">
+                <li>&copy; Rendact. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+});
+
+export default Home;
